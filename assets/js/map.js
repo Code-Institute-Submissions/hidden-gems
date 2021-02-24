328e1ee95addb632b301365c53314b079ad3626a
TomC2311 <tomaslcripps@gmail.com>
2021-02-24 19:37:47 +0000

    add castle locations object to map.js

--- assets/js/map.js
+++ assets/js/map.js
@@ -68,6 +68,74 @@ let beachLocations = [
 ]
 
 
+let castleLocations = [
+    {
+        name: "Doon Fort",
+        lat: 54.82915812360112,
+        lng: -8.464081532894864,
+        location: "Donegal"
+    },
+    {
+        name: "McDermott Castle",
+        lat: 53.98958226630207,
+        lng: -8.232584288008399,
+        location: "Roscommon"
+    },
+    {
+        name: `Cú Chulainn's Castle`,
+        lat: 54.01436057901478,
+        lng: -6.430028530692929,
+        location: "Louth"
+    },
+    {
+        name: "Kinbane Castle",
+        lat: 55.229378402935396,
+        lng: -6.291388746039393,
+        location: "Antrim"
+    },
+    {
+        name: "Drimnagh Castle",
+        lat: 53.325343615027215,
+        lng: -6.332674000000001,
+        location: "Dublin" 
+    },
+    {
+        name: "Leap Castle",
+        lat: 53.028377353540435,
+        lng: -7.808453400000001,
+        location: "Offaly"
+    },
+    {
+        name: "Cahir Castle",
+        lat: 52.37504489132966,
+        lng: -7.927203846039392,
+        location: "Tipperary"
+    },
+    {
+        name: "Rock of Cashel",
+        lat: 52.520572449407766,
+        lng: -7.890538030692929,
+        location: "Tipperary"
+    },
+    {
+        name: "Ross Castle",
+        lat: 52.04132859084017,
+        lng: -9.53143819,
+        location: "Kerry"
+    },
+    {
+        name: "Dunluce Castle",
+        lat: 55.21083931448046,
+        lng: -6.5796063,
+        location: "Antrim"
+    },
+    {
+        name: "Trim Castle",
+        lat: 53.55449698266906,
+        lng: -6.7893993000155834,
+        location: "Meath"
+    },
+]
 
 /* -- Function to run google map -- */
 

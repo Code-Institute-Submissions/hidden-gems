3bc91d778eeaa20a77564562f0dffa07348663b3
TomC2311 <tomaslcripps@gmail.com>
2021-03-04 18:57:07 +0000

    add message to contact form to indicate email has been sent successfully

--- assets/js/mail.js
+++ assets/js/mail.js
@@ -1,19 +1,23 @@
 
 console.log("hello is this working")
 
-function sendMail (contactForm) {
-    emailjs.send("service_f5w6jod","hidden-gems", {
+
+
+function sendMail(contactForm) {
+    emailjs.send("service_f5w6jod", "hidden-gems", {
         "from_name": contactForm.name.value,
         "from_email": contactForm.emailaddress.value,
         "location_suggestion": contactForm.locationsuggestion.value
     })
-    .then(
-        function(response) {
-            console.log("success", response)
-        },
-        function(error) {
-            console.log("failed", error)
-        }
-    );
+        .then(
+
+            function (response) {
+                console.log('success', response);
+                $('#success-message').show();
+            }, function (error) {
+                console.log('failed', error);
+                $('#error-message').show();
+            }
+        );
     return false;
 }

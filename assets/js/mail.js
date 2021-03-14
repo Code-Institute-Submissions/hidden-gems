e3741ecbad3496764a86f3006561cd65b0ec440a
TomC2311 <tomaslcripps@gmail.com>
2021-03-14 19:08:08 +0000

    create testing.md file, and move wireframes into correct folder within assets folder

--- assets/js/mail.js
+++ assets/js/mail.js
@@ -14,10 +14,10 @@ function sendMail(contactForm) {
 
             function (response) {
                 console.log('success', response);
-                $('#success-message').show("100");
+                $('#success-message').show("fast");
             }, function (error) {
                 console.log('failed', error);
-                $('#error-message').show("100");
+                $('#error-message').show("fast");
             }
         );
     document.getElementById("contactForm").reset();
@@ -34,7 +34,7 @@ function newsLetter(newsLetterForm) {
 
             function (response) {
                 console.log('success', response);
-                $("#newsletterThankYou").show("100");
+                $("#newsletterThankYou").show("fast");
             }, function (error) {
                 console.log('failed', error);
             }
